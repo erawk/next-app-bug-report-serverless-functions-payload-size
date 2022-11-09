5fa9204af197494472449a0028993399aa0147cd
Ed Lui <ed@erawk.ca>
2022-11-08 21:47:31 -0500

    Add dynamic page

---
+++ users/[id].tsx
@@ -0,0 +1,27 @@
+import { User } from '@prisma/client'
+import { GetStaticPropsContext } from 'next'
+import prisma from '../lib/database/prismaClient'
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
+  const id = (context.params as { id: string }).id
+  const user = await prisma.user.findFirst({ where: { id: parseInt(id, 10) } })
+
+  return {
+    props: {
+      id,
+      user,
+    },
+  }
+}
+
+function UserPage({ id, user }: { id: string; user: User }) {
+  return (
+    <>
+      <h1>The user id we are looking for is {id}.</h1>
+      <p>And the user details are:</p>
+      <pre>{JSON.stringify(user)}</pre>
+    </>
+  )
+}
+
+export default UserPage
