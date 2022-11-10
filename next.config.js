219c3ba27834dad0f7fe2556fede706ca14f638d
Ed Lui <ed@erawk.ca>
2022-11-10 09:43:24 -0500

    Add superjson

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import("next").NextConfig} */
 module.exports = {
+  experimental: {
+    swcPlugins: [['next-superjson-plugin', {}]], // Use SuperJSON in getStaticProps via SWC
+  },
   reactStrictMode: true,
 }
