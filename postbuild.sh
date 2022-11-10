f1f13eac9835f673d8c789c86d276d6bd8473acc
Ed Lui <ed@erawk.ca>
2022-11-10 10:24:07 -0500

    Fix sloppy typo

--- postbuild.sh
+++ postbuild.sh
@@ -8,5 +8,5 @@ echo "What is the state of @swc/core modules in node_modules/@swc/core-linux-x64
 DISK_USAGE_SWC_CORE=`du -h node_modules/@swc/core-linux-x64-*`
 echo "${DISK_USAGE_SWC_CORE}"
 echo "Here's the tree"
-TREE_SWC_CORE=`ls -lrtha nodule_modules/@swc/*`
+TREE_SWC_CORE=`ls -lrtha node_modules/@swc/*`
 echo "${TREE_SWC_CORE}"
